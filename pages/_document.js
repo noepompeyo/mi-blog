731a4c45ed67620a3515b8f4b338e749902a3753
noepm <pompeyonoe@gmail.com>
2023-03-19 00:15:07 -0600

    [ADD] se añadio la configuracion para google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-
+import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-F786M9B6B9"></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-F786M9B6B9');`,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
